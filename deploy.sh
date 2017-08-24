c0c35cb56764fc7437eeba622296154dd20a06c9
Paul Rowe <prowe@sourceallies.com>
2017-08-23 20:46:49 -0500

    getting aws creds works

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+set -e
+
+aws cloudformation update-stack \
+    --stack-name=prowe-push-iot \
+    --capabilities=CAPABILITY_IAM \
+    --template-body=file://cloudformation.template.yaml
